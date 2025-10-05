cfff3b355c94eee7d36c4de2de0d5e97504dc1d0
Omar-HA-Issa <oissa.ieu2023@student.ie.edu>
2025-10-05 21:31:01 +0200

    Final adjustments and details before submission of projec

--- backend/jest.setup.js
+++ backend/jest.setup.js
@@ -1,7 +1,6 @@
 // Jest setup file for test configuration
 // This file runs before all tests
 
-// Increase timeout for tests that involve database operations
 jest.setTimeout(10000);
 
 // Clean up after all tests complete
